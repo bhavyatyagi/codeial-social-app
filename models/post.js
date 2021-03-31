a298beee9d11b90e0af808390ee9795869fc9784
bhavyatyagi <bhavyatyagi16@gmail.com>
2021-03-31 20:22:25 +0530

    creating Schema for comments

--- models/post.js
+++ models/post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema({
     },
     // include the array of ids of all comments in the post schema itself
     comments: {
-        type: mongoose.schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment'
     }
 }, {
