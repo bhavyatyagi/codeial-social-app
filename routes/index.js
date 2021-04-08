b465c791c522b4b0299195c1bd72bf827d533370
Bhavya Tyagi <bhavyatyagi16@gmail.com>
2021-04-08 21:03:58 +0530

    add animation for notification using noty js

--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,11 @@ router.use('/comments', require('./comments'));
 //for an further routes, access from here
 //example: router.use('/routerName',require('./routerFile));
 module.exports = router;
+
+
+// Using connect-flash and noty js 
+// 1. isntall connect flash and app.use it after session initialisation
+// 2. Make a custom middleware in config and use it as centralised middleware to send msgs as res to locals
+// 3. ADd noty js CDN js and css min 
+// 4. instead of making and h tag make new noti({}).show()  in ejs files
+// 5. Finally send msg using req.flash('type','msg') from controllers
