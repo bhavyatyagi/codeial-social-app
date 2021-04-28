b1ad5682a7e1a04260f883c4dd825bc8cae8234d
Bhavya Tyagi <bhavyatyagi16@gmail.com>
2021-04-28 21:57:44 +0530

    add send email template ejs using nodemailer

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,14 +3,17 @@ const nodeMailer = require('../config/nodemailer');
 // this is another way of exporting a method 
 // this will be used / called in comments controller
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer');
+    // already defined /views/mailers in nodemailer.js 
+    let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
+
 
 
     nodeMailer.transporter.sendMail({
         from: 'noreply@codeial.com',
         to: comment.user.email,
         subject: 'Your post has new Comments',
-        html: '<h1>Your comment is published</h1>'
+        html: htmlString
+        // html: '<h1>Your comment is published</h1>'
 
     }, (error, info) => {
         if (error) {
